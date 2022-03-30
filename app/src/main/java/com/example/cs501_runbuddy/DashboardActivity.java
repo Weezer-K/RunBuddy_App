0a92db18556c8764d34ef9be01c48c32a8cebc88
Paul Menexas <pmenexas13@gmail.com>
2022-03-30 18:16:16 -0400

    added gps reading code

--- app/src/main/java/com/example/cs501_runbuddy/DashboardActivity.java
+++ app/src/main/java/com/example/cs501_runbuddy/DashboardActivity.java
@@ -1,13 +1,26 @@
 package com.example.cs501_runbuddy;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,10 +29,24 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 
 public class DashboardActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private LocationRequest locationRequest;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private LocationCallback locationCallBack;
+
+    private Location currentLocation;
+    private ArrayList<Location> savedLocations;
+
+    private final int DEFAULT_UPDATE_INTERVAL = 30;
+    private final int FASTEST_UPDATE_INTERVAL = 5;
+    private final int PERMISSIONS_FINE_LOCATION = 99;
 
     private GoogleMap mapAPI;
     private SupportMapFragment mapFragment;
@@ -38,6 +65,106 @@ public class DashboardActivity extends FragmentActivity implements OnMapReadyCal
         mapFragment.getMapAsync(this);
 
         nextLoc = (Button) findViewById(R.id.nexLoc);
+
+        savedLocations = new ArrayList<Location>();
+
+        locationRequest = LocationRequest.create()
+                .setInterval(1000 * DEFAULT_UPDATE_INTERVAL)
+                .setFastestInterval(1000 * FASTEST_UPDATE_INTERVAL)
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setMaxWaitTime(1000 * DEFAULT_UPDATE_INTERVAL);
+
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                updateUIWithLocation(locationResult.getLastLocation());
+            }
+        };
+
+        Switch sw_gps = new Switch(this);
+
+        sw_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sw_gps.isChecked()) {
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                } else {
+                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                }
+            }
+        });
+
+        Switch sw_locationUpdates = new Switch(this);
+
+        sw_locationUpdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sw_locationUpdates.isChecked()) {
+                    startLocationUpdates();
+                } else {
+                    stopLocationUpdates();
+                }
+            }
+        });
+
+        updateGPS();
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+        updateGPS();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateGPS();
+                } else {
+                    Toast.makeText(this, "This app requires permission to be granted" +
+                            " in order to work properly", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void updateGPS() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
+                DashboardActivity.this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this,
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            updateUIWithLocation(location);
+                            currentLocation = location;
+                            savedLocations.add(location);
+                        }
+                    });
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_FINE_LOCATION);
+            }
+        }
+    }
+
+    private void updateUIWithLocation(Location location) {
+        location.getLatitude();
+        location.getLongitude();
     }
 
 
@@ -66,5 +193,27 @@ public class DashboardActivity extends FragmentActivity implements OnMapReadyCal
                 mapAPI.moveCamera(CameraUpdateFactory.newLatLng(Point));
             }
         });
+    }
+
+    // given two coordinates, calculate the distance in miles
+    private double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        return (dist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
     }
 }
